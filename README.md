726d65036f7cc133918b6896d3c4315c67e73f7b
cse18107 <cse18107@cemk.ac.in>
2024-06-08 07:56:06 +0530

    added strucutre, convix and clerk to the project

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # ***AudioNest***
+
+28:25
+
+1:06:55
+
+
+npx convex dev
+
+npm run dev
+
+
+1:35:19
---
+++ convex/auth.config.ts
@@ -0,0 +1,12 @@
+// https://champion-mudfish-36.clerk.accounts.dev
+
+const authConfig = {
+    providers: [
+        {
+            domain: "https://champion-mudfish-36.clerk.accounts.dev",
+            applicationID: "convex"
+        },
+    ]
+};
+
+export default authConfig;
